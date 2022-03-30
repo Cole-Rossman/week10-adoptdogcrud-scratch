adca188054cbf8ef5960fbfa6f95776371c907a6
Cole Rossman <colerossman99@gmail.com>
2022-03-30 11:55:19 -0700

    Auth page added to app.js. state set and setuser added to header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import NavHeader from './components/Controls/NavHeader';
 import Home from './views/Home/DogList';
 import DogDetail from './views/Home/DogDetail';
 import NewPage from './views/Home/NewPage';
 import EditPage from './views/Home/EditPage';
+import { getUser } from './services/users';
+import Auth from './views/Auth';
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+
   return (
     <BrowserRouter>
-      <NavHeader />
+      <NavHeader currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <div className="App">
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/auth">
+            <Auth setCurrentUser={setCurrentUser} />
           </Route>
           <Route exact path="/dogs/new">
             <NewPage />
